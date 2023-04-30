4cc0fb7901b61561eb9658020c791c194231cb87
nurullah91 <mdnurullah.ptk@gmail.com>
2023-04-30 10:07:07 +0600

    editors insight and news details addedd

--- src/Pages/News/News/News.jsx
+++ src/Pages/News/News/News.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+import { FaArrowLeft } from 'react-icons/fa';
+import { Link, useLoaderData } from 'react-router-dom';
+import EditorsInsights from '../EditorsInsights/EditorsInsights';
 
 const News = () => {
+    const news = useLoaderData();
+    const {image_url, title, details, category_id} = news
+
     return (
         <div>
-            <h3>News details coming soon</h3>
+            <h3>Dragon News</h3>
+
+            <Card>
+                <Card.Img variant="top" src={image_url} />
+                <Card.Body>
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Text>
+                       {details}
+                    </Card.Text>
+                  <Link to={`/category/${category_id}`}><Button variant="danger"><FaArrowLeft></FaArrowLeft> All news in this category</Button></Link>
+                </Card.Body>
+            </Card>
+
+            <EditorsInsights></EditorsInsights>
         </div>
     );
 };
